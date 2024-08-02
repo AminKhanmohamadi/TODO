d0e2c26834856e1efb6e866c78c18eca6f8927a6
amin <aminkhm097@gmail.com>
2024-08-02 18:57:28 +0330

    first

--- todo/models.py
+++ todo/models.py
@@ -8,5 +8,8 @@ class Todo(models.Model):
     priority = models.IntegerField(default=1)
     is_done = models.BooleanField(default=False)
 
+    def __str__(self):
+        return f'{self.title}  / Is Done: {self.is_done}'
+
     class Meta:
         db_table = 'todos'
